c5fb650a667a879ec8cea0fdf666d45afb02da67
ajay <ajaynairarun@gmail.com>
2023-06-28 17:24:48 +0530

    add ; empty activity basic

---
+++ application/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("Hello World!");
+        setContentView(R.layout.activity_main);
+    }
+}
